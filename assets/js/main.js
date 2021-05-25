9f14b1f2efb9b0b2e0fe3a34cea38816494518d3
Emily <emiiiiiiin7@gmail.com>
2021-05-25 13:37:04 +0900

    JS追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,13 @@
-$(function () {
-   console.log("hello");
 
+$( '.js-input' ).keyup(function() {
+    if( $(this).val() ) {
+       $(this).addClass('not-empty');
+    } else {
+       $(this).removeClass('not-empty');
+    }
+  });
+
+$(".main3 > h3").on("mouseover",function(){
+   $(".main3 > div").slideDown();
    
-
-
-// $( '.js-input' ).keyup(function() {
-//     if( $(this).val() ) {
-//        $(this).addClass('not-empty');
-//     } else {
-//        $(this).removeClass('not-empty');
-//     }
-//   });
-
-
 });
